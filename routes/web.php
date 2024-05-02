38302a4ea2fbaa97029ff770587134809139a17d
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-05-02 15:50:26 +0300

    multiLang

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+// use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::get('/', function () {
+            return view('dashboard');
+        });
+    });
 
-Route::get('/', function () {
-    return view('dashboard');
-});
